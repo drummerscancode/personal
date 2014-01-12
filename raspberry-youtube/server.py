800036fa54f0dc76b39f132ed95eed7a8d6fd688
spygi <spygiannakis@hotmail.com>
2014-01-12 13:16:01 +0100

    delayed killing of the previous video

--- raspberry-youtube/server.py
+++ raspberry-youtube/server.py
@@ -10,35 +10,53 @@ class HelloResource(resource.Resource):
 	"""
 	mplayer -cookies -cookies-file /tmp/cookie.txt $(youtube-dl -g --cookies /tmp/cookie.txt "http://www.youtube.com/watch?v=kww0WXcH74o")
 	"""   
-	isLeaf = True
-	yt_dl = {} #
-	player = {} #
+	isLeaf = True # required don't know what it is
+	previous = { # previous video
+		'yt_dl': {},
+		'player': {}
+	}
 	base_url = 'http://www.youtube.com/watch?v='
     
 	def render_GET(self, request):
-		""" copy the play_url method from __init__.py
+		""" 
+		copy the play_url method from __init__.py
 		"""
 		print(request.args)
-		param = request.args['v'][0]
+		# TODO make it more generic by iterating the args?
+		# TODO add support for this
+		v_param = request.args['v'][0]
+		try:
+			start_param = request.args['start_at'][0]
+			end_param = request.args['end_at'][0]
+		except KeyError:
+			pass # just play the whole thing
+	
 		request.setHeader("content-type", "text/plain")
 
-		while self.player and self.player.poll() == None:
-			print('killing previous video')
-			os.killpg(self.player.pid, signal.SIGTERM)
-			print(self.player.poll())
-	
-		self.yt_dl = subprocess.Popen(['youtube-dl', '-g', self.base_url + param], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-		(yt_url, err) = self.yt_dl.communicate()
-		if self.yt_dl.returncode != 0:
-			return('Something\'s wrong with the retrieval of the url ' + sys.stderr.write(err))
+		# fine the new video
+		yt_dl = subprocess.Popen(['youtube-dl', '-g', self.base_url + v_param], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+		(yt_url, err) = yt_dl.communicate()
+		if yt_dl.returncode != 0:
+			return('Something\'s wrong with the retrieval of the url ' + err)
+			# sys.stderr.write(err)
 			#raise RuntimeError('Error getting URL ' + param)
 		
-		self.player = subprocess.Popen(
+		player = subprocess.Popen(
             ['omxplayer','-ohdmi', yt_url.decode('UTF-8').strip()],
             stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn=os.setsid)
+		print(player.poll()) # should be None if it's started
 
-		print(self.player.poll())
-		return "I play " + param + "\n"
+		# now kill the previous one
+		while self.previous['player'] and self.previous['player'].poll() == None:
+			print('killing previous video')
+			os.killpg(self.previous['player'].pid, signal.SIGTERM)
+			print(self.previous['player'].poll())
+
+		# update the previous
+		self.previous['yt_dl'] = yt_dl
+		self.previous['player'] = player
+		
+		return "I play " + v_param + "\n"
 	
 
 reactor.listenTCP(8080, server.Site(HelloResource()))
