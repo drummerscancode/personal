b4d8a3371a25febe19a2727de0d0ec35c1670e1e
SG <sg@doodle.com>
2015-02-13 10:32:31 +0100

    tnx to @spyros86 printf works, changed it to print only time if alreadyloggedintoday

--- timetrckr/app.sh
+++ timetrckr/app.sh
@@ -4,8 +4,6 @@ FILE=time.csv
 TODAY=`date "+%Y-%m-%d"`
 TIME=`date "+%H:%M:%S"`
 
-
-
 [ ! -f $FILE ] && echo "File $FILE not found!\n Creating it now!" && touch $FILE
 
 ALREADYLOGGEDINTODAY=`grep $TODAY $FILE`
@@ -13,8 +11,7 @@ ALREADYLOGGEDINTODAY=`grep $TODAY $FILE`
 if [ -z "$ALREADYLOGGEDINTODAY" ]; then 
         # It's a new day, you look great today
         printf "%s %s;" "$TODAY" "$TIME" > $FILE
-        #echo -n $TODAY $TIME > $FILE
 else
-        printf "%s %s;" "$TODAY" "$TIME" >> $FILE
-        #echo -n ";$TIME" >> $FILE
+		# print only the time
+        printf "%s;" "$TIME" >> $FILE
 fi
